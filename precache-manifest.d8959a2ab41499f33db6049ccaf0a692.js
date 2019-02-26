457b7514563f539d1dcb5749b8be8a93a985725c
mtanaya <tanayamichael@yahoo.com>
2019-02-26 15:33:01 -0800

    Updates

---
+++ precache-manifest.d8959a2ab41499f33db6049ccaf0a692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "caff8f4450a1e5b1c09b",
+    "url": "/my-app/static/js/runtime~main.caff8f44.js"
+  },
+  {
+    "revision": "36c2ed86c3c3afa65c18",
+    "url": "/my-app/static/js/main.36c2ed86.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/my-app/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "36c2ed86c3c3afa65c18",
+    "url": "/my-app/static/css/main.b0c5faa0.chunk.css"
+  },
+  {
+    "revision": "abb3377bad2f44bef752e5993a8df9bd",
+    "url": "/my-app/index.html"
+  }
+];
